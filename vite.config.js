3fec91587d5c49ec8d94a633aa742804fc6c89d9
vladislavgradoblyanskyi <vladislavgradoblyanskyi@gmail.com>
2026-02-15 10:53:17 +0100

    try

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
+import { defineConfig } from 'vite';
+import { glob } from 'glob';
+import injectHTML from 'vite-plugin-html-inject';
+import FullReload from 'vite-plugin-full-reload';
+import SortCss from 'postcss-sort-media-queries';
+
 export default defineConfig(({ command }) => {
   return {
     base: '/goit-js-hw-11/',   // ВОТ ЭТО ГЛАВНОЕ
